626f1d4a246003c85705d76812cb2d28880f9fcd
casSiANc02769 <malls_strata.0i@icloud.com>
2025-08-12 22:00:37 +0100

    Enhance new 密码加密解密工具 feature

---
+++ password_encryption_decryption_0813_0500_adt.vue
@@ -0,0 +1,40 @@
+// 代码生成时间: 2025-08-13 05:00:36
+import { ref, onMounted } from 'vue';
+
+export default {
+  name: 'PasswordEncryptionDecryption',
+  data() {
+    return {
+      inputText: '',
+      processedText: ''
+    };
+  },
+  setup() {
+    const inputText = ref('');
+    const processedText = ref('');
+    
+    const encrypt = () => {
+      processedText.value = Buffer.from(inputText.value).toString('base64');
+    };
+    
+    const decrypt = () => {
+      processedText.value = Buffer.from(processedText.value, 'base64').toString();
+    };
+    
+    onMounted(() => {
+      // Any initialization code goes here
+    });
+    
+    return {
+      inputText,
+      processedText,
+      encrypt,
+      decrypt
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* styles go here */
+</style>
