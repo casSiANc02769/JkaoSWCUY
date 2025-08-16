921c2baf084524972867c9a61f48c0f3ae2e2e21
casSiANc02769 <malls_strata.0i@icloud.com>
2025-08-16 11:46:31 +0100

    Add JS 用户界面组件库

---
+++ ui_component_library_0816_1846_yts.vue
@@ -0,0 +1,64 @@
+// 代码生成时间: 2025-08-16 18:46:30
+import { ref, onMounted } from 'vue';
+
+// 响应式数据
+const components = ref([
+  { name: 'Button', props: ['size', 'color', 'disabled'] },
+  { name: 'Input', props: ['type', 'placeholder', 'disabled'] },
+  { name: 'Checkbox', props: ['label', 'disabled'] },
+  // 其他组件...
+]);
+
+// 生命周期钩子
+onMounted(() => {
+  console.log('Component library UI component mounted.');
+});
+
+// 函数：添加组件
+function addComponent(component) {
+  components.value.push(component);
+}
+
+// 函数：删除组件
+function removeComponent(index) {
+  components.value.splice(index, 1);
+}
+
+export default {
+  components,
+  addComponent,
+  removeComponent,
+  onMounted
+};
+</script>
+
+<template>
+  <div class="ui-component-library">
+    <h1>User Interface Component Library</h1>
+    <ul>
+      <li v-for="(component, index) in components" :key="component.name">
+        {{ component.name }}
+        <button @click="removeComponent(index)">Remove</button>
+      </li>
+    </ul>
+    
+    <!-- 添加组件的表单 -->
+    <form @submit.prevent="addComponent({ name: componentName, props: componentProps })">
+      <input v-model="componentName" placeholder="Component Name" />
+      <input v-model="componentProps" placeholder="Props (comma separated)" />
+      <button type="submit">Add Component</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.ui-component-library {
+  padding: 20px;
+  max-width: 600px;
+  margin: auto;
+}
+.ui-component-library h1 {
+  text-align: center;
+}
+/* 其他样式... */
+</style>
