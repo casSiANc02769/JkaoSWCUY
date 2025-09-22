f2b11f10e86535526362d88506c553d7b1a7c0c9
casSiANc02769 <malls_strata.0i@icloud.com>
2025-09-22 07:58:32 +0100

    Create new 集成测试工具 feature

---
+++ test_integration_vue_component_0922_1458_ifc.vue
@@ -0,0 +1,26 @@
+// 代码生成时间: 2025-09-22 14:58:30
+import { ref, onMounted } from 'vue';
+
+// 响应式数据
+const message = ref('Hello, Vue 3!');
+
+// 生命周期钩子
+onMounted(() => {
+  console.log('Component is mounted.');
+});
+
+// 方法
+function handleClick() {
+  message.value = 'Button was clicked!';
+}
+
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+button {
+  margin-top: 20px;
+}
+</style>
